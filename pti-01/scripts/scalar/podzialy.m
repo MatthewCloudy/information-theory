cb66218d65b753c672185d83a545ac47f4c045e0
Michał Wdowski <michal.wdowski99@gmail.com>
2021-05-02 18:41:17 +0200

    CHanges

--- pti-01/scripts/scalar/podzialy.m
+++ pti-01/scripts/scalar/podzialy.m
@@ -5,5 +5,5 @@ function[partition, codebook] = podzialy(signal, n)
     
     partition = linspace(min_value, max_value, n);
     difference = partition(2) - partition(1);
-    codebook = linspace(min_value + difference/2, max_value + difference/2, n - 1);
+    codebook = linspace(min_value + difference/2, max_value - difference/2, n - 1);
 end
