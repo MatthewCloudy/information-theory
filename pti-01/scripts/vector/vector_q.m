da140e2c8edda0691f8794f57bc12f329f7c9066
Julia Borkowska <julia.borkowska@leancode.pl>
2021-05-03 15:51:44 +0200

    Add calculating mse for vector quantisation

--- pti-01/scripts/vector/vector_q.m
+++ pti-01/scripts/vector/vector_q.m
@@ -2,8 +2,11 @@ function [signal_quantized, mse] = vector_q(signal, n, vectorLength)
     vectors = splitVector(signal,vectorLength);
     codebook = fillCodeBook(n,vectors);
     quantisizedVectors = quantisize(codebook, vectors);
-    signal_quantized = zeros(l+vectorLength,2);
+    signal_quantized = zeros(size(quantisizedVectors,1) * size(quantisizedVectors,2),1);
     for i = 1:size(quantisizedVectors,2)
         signal_quantized( ((i-1)*vectorLength + 1):(i*vectorLength), 1 ) = quantisizedVectors(:,i)';
     end
+    %mse = zeros(l+vectorLength,2);
+    padded_signal = [signal zeros(length(signal_quantized) - length(signal))];
+    mse = realsqrt( (padded_signal - signal_quantized).^2./length(signal_quantized) );
 end
