b7092417e7a2b16b0bf54f0cff45554a1c291b4d
Julia Borkowska <julia.borkowska@leancode.pl>
2021-05-03 21:01:21 +0200

    Remove unnecessary files again

--- pti-01/wektorowa/fillCodeBook.m
+++
@@ -1,6 +0,0 @@
-function [codebook] = fillCodeBook(n, vectors)
-    codebook = zeros(n, size(vectors,1));
-    for i = 1:n
-        codebook(i,:) = vectors(:,randi([ceil((i-1) * size(vectors,2) / n + 1), ceil(i * size(vectors,2) / n)],1,1))';
-    end
-end
--- pti-01/wektorowa/splitVector.m
+++
@@ -1,6 +0,0 @@
-function [division] = splitVector(v, n)
-    division = zeros(n, ceil(length(v)/n));
-    for i = 1:length(v)
-        division(mod(i,n) + 1, ceil(i/n)) = v(i);
-    end
-end
