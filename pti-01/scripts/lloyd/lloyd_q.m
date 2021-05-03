b69ce4b5cb9b254be16278a52983657d72715d2c
Mateusz Chmurzyński <83476350+MatthewCloudy@users.noreply.github.com>
2021-05-03 11:36:56 +0200

    Add files via upload

---
+++ pti-01/scripts/lloyd/lloyd_q.m
@@ -0,0 +1,20 @@
+function [signal_quantized, mse] = lloyd_q(signal, n)
+% LLOYD_Q
+%   Argumenty:
+%       - signal - oryginalny sygnał
+%       - n - liczba przedziałów na które dzielimy zbiór wartości
+%   Zwraca:
+%       - signal_quantized - sygnał po kwantyzacji
+%       - mse - błąd średniokwadratowy sygnału po kwantyzacji względem
+%       oryginalnego
+
+% Funkcja lloyds() z pakietu Communications Toolbox
+[partition, codebook] = lloyds(signal, n);
+
+% Kwantyzacja i obliczenie błędu
+lloyd_q = dsp.ScalarQuantizerEncoder("BoundaryPoints", partition,...
+    "CodewordOutputPort", true(1), "Codebook", codebook);
+[~, signal_quantized] = lloyd_q(signal);
+mse = mean((signal - signal_quantized).^2);
+
+end
